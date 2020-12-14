0a52c96e846c965dfae58d48195834d5dbab1ca2
tjoeun <tjoeun@DESKTOP-PA16CCR>
2020-12-14 17:11:48 +0900

    spring00 file upload!

---
+++ src/main/java/com/mega/mvc01/MemberDAO.java
@@ -0,0 +1,20 @@
+package com.mega.mvc01;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MemberDAO {
+	
+	@Autowired
+	SqlSessionTemplate my;
+	
+	public void insert(MemberDTO dto) {
+		my.insert("member.insert", dto);
+	}
+}
+
+
+
+
